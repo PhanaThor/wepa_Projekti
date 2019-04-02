cdff51bc17e6cf8f52e13192619184a078ba7db2
Tommi Laakso <laakso.tommi@gmail.com>
2019-04-02 13:24:59 +0300

    Initial commit
    Initial commit

---
+++ src/main/java/fi/dy/laakso/vanhankansanfacebook/VanhankansanfacebookApplication.java
@@ -0,0 +1,13 @@
+package fi.dy.laakso.vanhankansanfacebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanhankansanfacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanhankansanfacebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/dy/laakso/vanhankansanfacebook/VanhankansanfacebookApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.dy.laakso.vanhankansanfacebook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VanhankansanfacebookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
