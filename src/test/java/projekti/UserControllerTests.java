500ca4ea11b7650dfa2abf1ebb168e391b99e970
Tommi Laakso <laakso.tommi@gmail.com>
2019-04-06 01:30:27 +0300

    Update UserControllerTests.java
    Added database cleanup before each test.

--- src/test/java/projekti/UserControllerTests.java
+++ src/test/java/projekti/UserControllerTests.java
@@ -10,6 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import projekti.repositories.UserRepository;
+
 import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,6 +27,13 @@ public class UserControllerTests {
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public void Init() {
+		userRepository.deleteAll();
+	}
 
 	@Test
 	public void canGetUsers() throws Exception {
