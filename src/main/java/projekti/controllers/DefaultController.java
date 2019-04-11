d41afd58ca9e5a482f5b7c5c3326e0f66de6d711
Tommi Laakso <laakso.tommi@gmail.com>
2019-04-12 00:11:18 +0300

    Update DefaultController.java

--- src/main/java/projekti/controllers/DefaultController.java
+++ src/main/java/projekti/controllers/DefaultController.java
@@ -2,6 +2,8 @@ package projekti.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
 @Controller
@@ -11,5 +13,22 @@ public class DefaultController {
     public String getIndex(Model model) {
         model.addAttribute("loggedUserProfileStr", "MrSample");
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(Model model) {        
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(@RequestParam String username, @RequestParam String password) {
+        System.out.println("UserName: " + username);
+        System.out.println("Password: " + password);
+        return "index";
+    }
+
+    @GetMapping("/logout")
+    public String getLogout(Model model) {        
+        return "redirect:/login";
     }
 }
