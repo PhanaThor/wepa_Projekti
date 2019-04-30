8bfc42a6f96156f8fe35a8baadca5c5caef5546a
Tommi Laakso <laakso.tommi@gmail.com>
2019-04-30 09:50:50 +0300

    Deleted unused tests.

--- src/test/java/projekti/AccountControllerTests.java
+++ src/test/java/projekti/AccountControllerTests.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-public class AccountControllerUnitTests {
+public class AccountControllerTests {
 
 	private final String API_URI = "/users";
 
